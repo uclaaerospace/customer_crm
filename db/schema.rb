d6413af627c1dba509cee93eb971d158fdc8269a
Yoshihisa Haruyama <uclaaerospace@gmail.com>
2015-11-23 14:23:52 +0900

    Company nameを必須記入に変更。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123004910) do
+ActiveRecord::Schema.define(version: 20151123052120) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "url"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "family_name", null: false
