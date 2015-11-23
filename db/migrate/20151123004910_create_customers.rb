31a0a9633f8227fbf86b29446d959f5350281900
Yoshihisa Haruyama <uclaaerospace@gmail.com>
2015-11-23 09:55:27 +0900

    顧客情報の氏名を上限20文字制限追加 氏名、メールアドレス必須 rails g controller customers index new create edit update show destroy

--- db/migrate/20151123004910_create_customers.rb
+++ db/migrate/20151123004910_create_customers.rb
@@ -1,11 +1,13 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.string :family_name
-      t.string :given_name
-      t.string :email
+      t.string :family_name, null: false, length: { maximum: 20 }
+      t.string :given_name,  null: false, length: { maximum: 20 }
+      t.string :email,       null: false
 
       t.timestamps null: false
     end
+    add_index :customers, :email, unique: true
   end
 end
+
